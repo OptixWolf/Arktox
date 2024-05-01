a02101ee167b6c2143b27918a26cb9f78029949b
OptixWolf <62619140+OptixWolf@users.noreply.github.com>
2024-05-01 20:52:41 +0200

    Database class moved to database.dart

---
+++ lib/database.dart
@@ -0,0 +1,31 @@
+import 'package:mysql_client/mysql_client.dart';
+import 'package:http/http.dart' as http;
+import 'keys.dart';
+
+class DatabaseService {
+  late MySQLConnection _conn;
+
+  Future<void> connect() async {
+    _conn = await MySQLConnection.createConnection(
+      host: host,
+      port: port,
+      userName: user,
+      password: password,
+      databaseName: databaseName,
+      secure: false,
+    );
+
+    await _conn.connect();
+    print('Datenbank verbunden');
+  }
+
+  Future<void> disconnect() async {
+    await _conn.close();
+    print('Datenbank getrennt');
+  }
+
+  Future<List<Map<String, dynamic>>> executeQuery(String query) async {
+    var result = await _conn.execute(query);
+    return result.rows.map((row) => row.assoc()).toList();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:mysql_client/mysql_client.dart';
-import 'package:http/http.dart' as http;
-import 'keys.dart';
+import 'database.dart';
 
 void main() {
   DatabaseService().connect();
   runApp(const ArktoxApp());
-}
-
-class DatabaseService {
-  late MySQLConnection _conn;
-
-  Future<void> connect() async {
-    _conn = await MySQLConnection.createConnection(
-      host: host,
-      port: port,
-      userName: user,
-      password: password,
-      databaseName: databaseName,
-      secure: false,
-    );
-
-    await _conn.connect();
-    print('Datenbank verbunden');
-  }
-
-  Future<void> disconnect() async {
-    await _conn.close();
-    print('Datenbank getrennt');
-  }
-
-  Future<List<Map<String, dynamic>>> executeQuery(String query) async {
-    var result = await _conn.execute(query);
-    return result.rows.map((row) => row.assoc()).toList();
-  }
 }
 
 class ArktoxApp extends StatelessWidget {
