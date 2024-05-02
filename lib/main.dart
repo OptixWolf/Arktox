bb71e85348eccf8637875f60d00f07b348bf4002
OptixWolf <62619140+OptixWolf@users.noreply.github.com>
2024-05-02 12:19:12 +0200

    Profilseite hinzugefügt

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:crypto/crypto.dart';
@@ -78,6 +79,8 @@ class _HomepageState extends State<Homepage> {
   String email = '';
   String passwordHash = '';
   bool loggedIn = false;
+  int own_profile_id = -1;
+  int own_user_id = -1;
 
   @override
   void initState() {
@@ -98,12 +101,23 @@ class _HomepageState extends State<Homepage> {
         print(emailValue);
         passwordHash = passwordHashValue;
         print(passwordHashValue);
-        var result = DatabaseService()
+        DatabaseService()
             .executeQuery(
                 'SELECT user_id FROM nutzerdaten WHERE email = \'$emailValue\' AND password_hash = \'$passwordHashValue\'')
             .then((result) {
           if (result.isNotEmpty) {
             loggedIn = true;
+            own_user_id = int.parse(result.first['user_id']);
+            DatabaseService()
+                .executeQuery(
+                    'SELECT profile_id FROM profil WHERE user_id = $own_user_id')
+                .then(
+              (value) {
+                if (value.isNotEmpty) {
+                  own_profile_id = int.parse(value.first['profile_id']);
+                }
+              },
+            );
             print(result.first);
           }
         });
@@ -192,7 +206,7 @@ class _HomepageState extends State<Homepage> {
                     child: ListTile(
                   title: Text(loggedIn ? 'Abmelden' : 'Anmelden'),
                   subtitle: Text(loggedIn
-                      ? 'Aktuell angemeldet mit: $email'
+                      ? 'Aktuell angemeldet mit: \n$email'
                       : 'Aktuell nicht angemeldet'),
                   trailing: IconButton(
                     icon: const Icon(Icons.arrow_right),
@@ -207,7 +221,40 @@ class _HomepageState extends State<Homepage> {
                       }
                     },
                   ),
+                  onTap: () {
+                    if (loggedIn) {
+                      Preferences.setPrefString('email', '');
+                      Preferences.setPrefString('passwordHash', '');
+                      Phoenix.rebirth(context);
+                    } else {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => LoginPage()));
+                    }
+                  },
                 )),
+                Visibility(
+                  visible: loggedIn,
+                  child: Card(
+                      child: ListTile(
+                    title: const Text('Profil'),
+                    subtitle: const Text('Zeigt dir dein Profil'),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.arrow_right),
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => ProfilePage(
+                                  profile_id: own_profile_id,
+                                )));
+                      },
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => ProfilePage(
+                                profile_id: own_profile_id,
+                              )));
+                    },
+                  )),
+                ),
                 const SizedBox(
                   height: 25,
                 ),
@@ -265,8 +312,11 @@ class LoginPage extends StatelessWidget {
     String passwordHash = '';
 
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Anmelden/Registrieren'),
+      ),
       body: Padding(
-        padding: EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(20.0),
         child: Center(
           child: SingleChildScrollView(
             child: Column(
@@ -315,7 +365,7 @@ class LoginPage extends StatelessWidget {
                           });
                         },
                         color: Colors.blue,
-                        child: Text('Anmelden'),
+                        child: const Text('Anmelden'),
                       ),
                     ),
                     const SizedBox(width: 10),
@@ -325,16 +375,101 @@ class LoginPage extends StatelessWidget {
                           //
                         },
                         color: Colors.grey,
-                        child: Text('Registrieren'),
+                        child: const Text('Registrieren'),
                       ),
                     ),
                   ],
                 ),
+                const SizedBox(height: 40),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  final int profile_id;
+
+  const ProfilePage({super.key, required this.profile_id});
+
+  Future<List<Map<String, dynamic>>> getProfileDetails() async {
+    var result = await DatabaseService()
+        .executeQuery('SELECT * FROM profil WHERE profile_id = $profile_id');
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profilseite'),
+      ),
+      body: FutureBuilder(
+          future: getProfileDetails(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Container();
+            } else if (snapshot.hasData) {
+              return Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20)),
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 200.0,
+                          child: Image.network(
+                            snapshot.data?.first['profilbanner_link'],
+                            fit: BoxFit.cover,
+                            loadingBuilder: (context, child, loadingProgress) {
+                              if (loadingProgress == null) return child;
+                              return const CircularProgressIndicator();
+                            },
+                            errorBuilder: (context, error, stackTrace) =>
+                                SizedBox(
+                              width: double.infinity,
+                              height: 200.0,
+                              child: Image.asset(
+                                'assets/default_banner.jpg',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Card(
+                        margin: EdgeInsets.zero,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(20),
+                              bottomRight: Radius.circular(20)),
+                        ),
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                snapshot.data?.first['profilbild_link']),
+                          ),
+                          title: Text(snapshot.data?.first['username']),
+                          subtitle: Text(snapshot.data?.first['about_me']),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
+            throw ();
+          }),
     );
   }
 }
