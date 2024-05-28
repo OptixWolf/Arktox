5b0979aa51ffe410c3cf7eb35e25a10d1ca627a8
OptixWolf <62619140+OptixWolf@users.noreply.github.com>
2024-05-28 14:31:25 +0200

    Skript für anderes verbessert

--- scripts/arktox_insert_otherthings_script.sql
+++ scripts/arktox_insert_otherthings_script.sql
@@ -6,7 +6,7 @@ DROP VIEW IF EXISTS view_unbestaetigte_archiv_eintraege;
 DROP VIEW IF EXISTS view_unbestaetigte_skript_eintraege;
 DROP VIEW IF EXISTS  view_archiv_eintraege_approval_status;
 DROP VIEW IF EXISTS view_skripte_approval_status;
-DROP TRIGGER IF EXISTS before_update_profilbanner;
+DROP TRIGGER IF EXISTS before_update_profil;
 DROP ROLE IF EXISTS 'moderator';
 DROP USER IF EXISTS 'testuser'@'localhost';
 
@@ -36,13 +36,14 @@ FROM skripte;
 
 DELIMITER //
 
-CREATE TRIGGER before_update_profilbanner
+CREATE TRIGGER before_update_profil
 BEFORE UPDATE ON profil
 FOR EACH ROW
 BEGIN
-    DECLARE invalid_link BOOLEAN;
+    DECLARE invalid_profilbanner BOOLEAN;
+    DECLARE invalid_profilbild BOOLEAN;
 
-    SET invalid_link = 
+    SET invalid_profilbanner = 
         NEW.profilbanner_link LIKE '%iplogger.com%' OR
         NEW.profilbanner_link LIKE '%maper.info%' OR
         NEW.profilbanner_link LIKE '%iplogger.ru%' OR
@@ -63,8 +64,33 @@ BEGIN
         NEW.profilbanner_link LIKE '%ed.tc%' OR
         NEW.profilbanner_link LIKE '%bc.ax%';
 
-    IF invalid_link THEN
+    SET invalid_profilbild = 
+        NEW.profilbild_link LIKE '%iplogger.com%' OR
+        NEW.profilbild_link LIKE '%maper.info%' OR
+        NEW.profilbild_link LIKE '%iplogger.ru%' OR
+        NEW.profilbild_link LIKE '%iplogger.co%' OR
+        NEW.profilbild_link LIKE '%2no.co%' OR
+        NEW.profilbild_link LIKE '%yip.su%' OR
+        NEW.profilbild_link LIKE '%iplogger.info%' OR
+        NEW.profilbild_link LIKE '%ipgrabber.ru%' OR
+        NEW.profilbild_link LIKE '%ipgraber.ru%' OR
+        NEW.profilbild_link LIKE '%iplis.ru%' OR
+        NEW.profilbild_link LIKE '%02ip.ru%' OR
+        NEW.profilbild_link LIKE '%ezstat.ru%' OR
+        NEW.profilbild_link LIKE '%iplog.co%' OR
+        NEW.profilbild_link LIKE '%iplogger.cn%' OR
+        NEW.profilbild_link LIKE '%grabify.link%' OR
+        NEW.profilbild_link LIKE '%iplogger.org%' OR
+        NEW.profilbild_link LIKE '%wl.gl%' OR
+        NEW.profilbild_link LIKE '%ed.tc%' OR
+        NEW.profilbild_link LIKE '%bc.ax%';
+
+    IF invalid_profilbanner THEN
         SET NEW.profilbanner_link = OLD.profilbanner_link;
+    END IF;
+
+    IF invalid_profilbild THEN
+        SET NEW.profilbild_link = OLD.profilbild_link;
     END IF;
 END //
 
